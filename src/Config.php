fc39be5065a16462b5b4506dbec3119402b68a17
Mikhail Ilinsky <mail@milinsky.ru>
2023-04-12 21:47:39 +1000

    Add default return value

--- src/Config.php
+++ src/Config.php
@@ -4,23 +4,23 @@ declare(strict_types=1);
 
 namespace Duyler\Config;
 
-class Config
+readonly class Config
 {
-    private ConfigProviderInterface $provider;
-
-    public function __construct(ConfigProviderInterface $provider)
+    public function __construct(private ConfigProviderInterface $provider)
     {
-        $this->provider = $provider;
     }
 
-    public function get(string $configFile, string $configName): string | int | float | bool | null
-    {
+    public function get(
+        string $configFile,
+        string $configName,
+        string|int|float|bool|null $default = null
+    ): string|int|float|bool|null {
         $configArray = $this->provider->getAll($configFile);
 
         if (array_key_exists($configName, $configArray)) {
             return $configArray[$configName];
         }
 
-        return null;
+        return $default;
     }
 }
